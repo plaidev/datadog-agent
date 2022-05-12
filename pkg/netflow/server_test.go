252bc1d939a726e2f0d49d8681192deaa47741ea
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-05-12 15:46:17 +0200

    Try fix race cond

--- pkg/netflow/server_test.go
+++ pkg/netflow/server_test.go
@@ -1,16 +1,10 @@
 package netflow
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"github.com/DataDog/datadog-agent/pkg/aggregator"
 	"github.com/DataDog/datadog-agent/pkg/aggregator/mocksender"
 	"github.com/DataDog/datadog-agent/pkg/config"
-	"github.com/DataDog/datadog-agent/pkg/netflow/goflowlib"
-	"github.com/DataDog/datadog-agent/pkg/util/log"
-	"github.com/cihub/seelog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -20,31 +14,6 @@ import (
 )
 
 func TestNewNetflowServer(t *testing.T) {
-	// NetFlow5 example data from goflow repo:
-	// https://github.com/netsampler/goflow2/blob/5300494e478567a0fb9b8de0c504d9442260d0ad/decoders/netflowlegacy/netflow_test.go#L11-L32
-	netflowV5Data := []byte{
-		0x00, 0x05, 0x00, 0x06, 0x00, 0x82, 0xc3, 0x48, 0x5b, 0xcd, 0xba, 0x1b, 0x05, 0x97, 0x6d, 0xc7,
-		0x00, 0x00, 0x64, 0x3d, 0x08, 0x08, 0x00, 0x00, 0x0a, 0x80, 0x02, 0x79, 0x0a, 0x80, 0x02, 0x01,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x00, 0x02, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x02, 0x4e,
-		0x00, 0x82, 0x9b, 0x8c, 0x00, 0x82, 0x9b, 0x90, 0x1f, 0x90, 0xb9, 0x18, 0x00, 0x1b, 0x06, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x80, 0x02, 0x77, 0x0a, 0x81, 0x02, 0x01,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x94,
-		0x00, 0x82, 0x95, 0xa9, 0x00, 0x82, 0x9a, 0xfb, 0x1f, 0x90, 0xc1, 0x2c, 0x00, 0x12, 0x06, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x81, 0x02, 0x01, 0x0a, 0x80, 0x02, 0x77,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x07, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0xc2,
-		0x00, 0x82, 0x95, 0xa9, 0x00, 0x82, 0x9a, 0xfc, 0xc1, 0x2c, 0x1f, 0x90, 0x00, 0x16, 0x06, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x80, 0x02, 0x01, 0x0a, 0x80, 0x02, 0x79,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x09, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x01, 0xf1,
-		0x00, 0x82, 0x9b, 0x8c, 0x00, 0x82, 0x9b, 0x8f, 0xb9, 0x18, 0x1f, 0x90, 0x00, 0x1b, 0x06, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x80, 0x02, 0x01, 0x0a, 0x80, 0x02, 0x79,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x09, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x02, 0x2e,
-		0x00, 0x82, 0x9b, 0x90, 0x00, 0x82, 0x9b, 0x9d, 0xb9, 0x1a, 0x1f, 0x90, 0x00, 0x1b, 0x06, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x80, 0x02, 0x79, 0x0a, 0x80, 0x02, 0x01,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x00, 0x02, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x0b, 0xac,
-		0x00, 0x82, 0x9b, 0x90, 0x00, 0x82, 0x9b, 0x9d, 0x1f, 0x90, 0xb9, 0x1a, 0x00, 0x1b, 0x06, 0x00,
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-	}
-
 	// Setup NetFlow feature config
 	port := getFreePort()
 	config.Datadog.SetConfigType("yaml")
@@ -71,74 +40,18 @@ network_devices:
 	server, err := NewNetflowServer(demux)
 	require.NoError(t, err, "cannot start Netflow Server")
 	assert.NotNil(t, server)
-	defer server.stop()
 
 	// Send netflowV5Data twice to test aggregator
 	// Flows will have 2x bytes/packets after aggregation
-	err = sendUDPPacket(port, netflowV5Data)
+	err = sendUDPPacket(port, mockNetflowV5Data)
 	require.NoError(t, err)
-	err = sendUDPPacket(port, netflowV5Data)
+	err = sendUDPPacket(port, mockNetflowV5Data)
 	require.NoError(t, err)
 
 	// Make sure flows are flushed before doing assertions
 	waitFlowsToBeFlushed(server.flowAgg, 10*time.Second)
 
-	// Assertions
-
-	// language = json
-	event := []byte(`
-{
-  "type": "netflow5",
-  "sampling_rate": 0,
-  "direction": "ingress",
-  "start": 1540209168,
-  "end": 1540209169,
-  "bytes": 388,
-  "packets": 6,
-  "ether_type": 2048,
-  "ip_protocol": 6,
-  "tos": 0,
-  "exporter": {
-    "ip": "127.0.0.1"
-  },
-  "source": {
-    "ip": "10.129.2.1",
-    "port": 49452,
-    "mac": "00:00:00:00:00:00",
-    "mask": "0.0.0.0/24"
-  },
-  "destination": {
-    "ip": "10.128.2.119",
-    "port": 8080,
-    "mac": "",
-    "mask": ""
-  },
-  "ingress": {
-    "interface": {
-      "index": 1
-    }
-  },
-  "egress": {
-    "interface": {
-      "index": 7
-    }
-  },
-  "namespace": "default",
-  "host": "COMP-C02CF0CWLVDP",
-  "tcp_flags": [
-    "SYN",
-    "ACK"
-  ],
-  "next_hop": {
-    "ip": "0.0.0.0"
-  }
-}
-`)
-	compactEvent := new(bytes.Buffer)
-	err = json.Compact(compactEvent, event)
-	assert.NoError(t, err)
-
-	sender.AssertEventPlatformEvent(t, compactEvent.String(), "network-devices-netflow")
+	sender.AssertEventPlatformEvent(t, mock.Anything, "network-devices-netflow")
 	sender.AssertMetric(t, "Count", "datadog.newflow.aggregator.flows_received", 1, "", []string{"sample_addr:127.0.0.1", "flow_type:netflow5"})
 }
 
@@ -148,6 +61,8 @@ func TestStartServerAndStopServer(t *testing.T) {
 	err := StartServer(demux)
 	require.NoError(t, err)
 	require.NotNil(t, serverInstance)
+
+	replaceWithDummyFlowProcessor(serverInstance, 123)
 
 	StopServer()
 	require.Nil(t, serverInstance)
@@ -165,17 +80,10 @@ func TestIsEnabled(t *testing.T) {
 }
 
 func TestServer_Stop(t *testing.T) {
-	// Setup logger to record logs
-	var b bytes.Buffer
-	w := bufio.NewWriter(&b)
-	l, err := seelog.LoggerFromWriterWithMinLevelAndFormat(w, seelog.DebugLvl, "[%LEVEL] %FuncShort: %Msg")
-	assert.Nil(t, err)
-	log.SetupLogger(l, "debug")
-
 	// Setup NetFlow config
 	port := getFreePort()
 	config.Datadog.SetConfigType("yaml")
-	err = config.Datadog.MergeConfigOverride(strings.NewReader(fmt.Sprintf(`
+	err := config.Datadog.MergeConfigOverride(strings.NewReader(fmt.Sprintf(`
 network_devices:
   netflow:
     enabled: true
@@ -194,23 +102,11 @@ network_devices:
 	require.NoError(t, err, "cannot start Netflow Server")
 	assert.NotNil(t, server)
 
-	// Testing using a dummyFlowProcessor since we can't test using real goflow flow processor
-	// due to this race condition https://github.com/netsampler/goflow2/issues/83
-	flowProcessor := &dummyFlowProcessor{}
-	listener := server.listeners[0]
-	listener.flowState = &goflowlib.FlowStateWrapper{
-		State:    flowProcessor,
-		Hostname: "abc",
-		Port:     port,
-	}
+	flowProcessor := replaceWithDummyFlowProcessor(server, port)
 
 	// Stops server
 	server.stop()
 
 	// Assert logs present
-	w.Flush()
-	logs := b.String()
 	assert.Equal(t, flowProcessor.stopped, true)
-	assert.Equal(t, strings.Count(logs, fmt.Sprintf("Listener `0.0.0.0:%d` shutting down", port)), 1, logs)
-	assert.Equal(t, strings.Count(logs, fmt.Sprintf("Listener `0.0.0.0:%d` stopped", port)), 1, logs)
 }
