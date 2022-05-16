42e909c7f03f1e79d443636d19f5b637fe40bc24
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-05-16 10:53:38 +0200

    Fix lint

--- pkg/netflow/enrichment/mac.go
+++ pkg/netflow/enrichment/mac.go
@@ -5,6 +5,7 @@ import (
 	"net"
 )
 
+// FormatMacAddress formats mac address from uint64 to "xx:xx:xx:xx:xx:xx" format
 func FormatMacAddress(fieldValue uint64) string {
 	mac := make([]byte, 8)
 	binary.BigEndian.PutUint64(mac, fieldValue)
