3ffb1d2a717976170db62a5a9b873a844e398885
Alexandre Yang <alexandre.yang@datadoghq.com>
2022-05-16 19:17:26 +0200

    merge netflow_v4

--- pkg/netflow/flowaggregator/buildpayload.go
+++ pkg/netflow/flowaggregator/buildpayload.go
@@ -27,9 +27,9 @@ func buildPayload(aggFlow *common.Flow) payload.FlowPayload {
 
 	return payload.FlowPayload{
 		// TODO: Implement Tos
-		FlowType:          string(aggFlow.FlowType),
-		SamplingRate:      aggFlow.SamplingRate,
-		Direction:         enrichment.RemapDirection(aggFlow.Direction),
+		FlowType:     string(aggFlow.FlowType),
+		SamplingRate: aggFlow.SamplingRate,
+		Direction:    enrichment.RemapDirection(aggFlow.Direction),
 		Exporter: payload.Exporter{
 			IP: aggFlow.SamplerAddr,
 		},
